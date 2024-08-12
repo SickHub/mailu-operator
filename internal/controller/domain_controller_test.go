48c71f6cd8b55d3e27d8c8431a9a852e15f1a58e
Pascal Sick <muc@sicknet.de>
2024-08-12 15:38:34 +0000

    Initial commit

---
+++ internal/controller/domain_controller_test.go
@@ -0,0 +1,302 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	jsoniter "github.com/json-iterator/go"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"gitlab.rootcrew.net/rootcrew/services/mailu-operator/pkg/mailu"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"net/http"
+	"net/http/httptest"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	operatorv1alpha1 "gitlab.rootcrew.net/rootcrew/services/mailu-operator/api/v1alpha1"
+)
+
+var _ = Describe("Domain Controller", func() {
+	Context("When reconciling a created resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		domain := &operatorv1alpha1.Domain{}
+
+		mailuMock := mailuMock()
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind Domain")
+			err := k8sClient.Get(ctx, typeNamespacedName, domain)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &operatorv1alpha1.Domain{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					Spec: operatorv1alpha1.DomainSpec{
+						Name:          "nonexistent.com",
+						Comment:       "example domain",
+						MaxUsers:      -1,
+						MaxAliases:    -1,
+						MaxQuotaBytes: -1,
+						SignupEnabled: false,
+						Alternatives:  []string{},
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterEach(func() {
+			resource := &operatorv1alpha1.Domain{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance Domain")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the created resource")
+			controllerReconciler := &DomainReconciler{
+				Client:   k8sClient,
+				Scheme:   k8sClient.Scheme(),
+				ApiURL:   mailuMock,
+				ApiToken: "asdf",
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			domain := &operatorv1alpha1.Domain{}
+			err = k8sClient.Get(ctx, typeNamespacedName, domain)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(domain.Status.Conditions).ToNot(BeEmpty())
+		})
+	})
+
+	Context("When reconciling a deleted resource", func() {
+		const resourceName = "test-resource"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default", // TODO(user):Modify as needed
+		}
+		domain := &operatorv1alpha1.Domain{}
+
+		mailuMock := mailuMock()
+
+		BeforeEach(func() {
+			By("creating the custom resource for the Kind Domain")
+			err := k8sClient.Get(ctx, typeNamespacedName, domain)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &operatorv1alpha1.Domain{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					Spec: operatorv1alpha1.DomainSpec{
+						Name:          "example.com",
+						Comment:       "example domain",
+						MaxUsers:      -1,
+						MaxAliases:    -1,
+						MaxQuotaBytes: -1,
+						SignupEnabled: false,
+						Alternatives:  []string{},
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+
+				// reconcile to create the resource
+				controllerReconciler := &DomainReconciler{
+					Client:   k8sClient,
+					Scheme:   k8sClient.Scheme(),
+					ApiURL:   mailuMock,
+					ApiToken: "asdf",
+				}
+
+				_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+					NamespacedName: typeNamespacedName,
+				})
+				Expect(err).NotTo(HaveOccurred())
+
+				k8sClient.Get(ctx, typeNamespacedName, domain)
+				Expect(domain.Status.Conditions).ToNot(BeEmpty())
+				Expect(domain.ObjectMeta.Finalizers).ToNot(BeEmpty())
+
+				// delete the resource
+				Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+				k8sClient.Get(ctx, typeNamespacedName, domain)
+				Expect(domain.ObjectMeta.DeletionTimestamp).ToNot(BeNil())
+			}
+		})
+
+		AfterEach(func() {
+			// resource should not be found
+			resource := &operatorv1alpha1.Domain{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).To(HaveOccurred())
+			Expect(errors.IsNotFound(err)).To(BeTrue())
+		})
+
+		It("should successfully reconcile the resource", func() {
+			By("Reconciling the deleted resource")
+			controllerReconciler := &DomainReconciler{
+				Client:   k8sClient,
+				Scheme:   k8sClient.Scheme(),
+				ApiURL:   mailuMock,
+				ApiToken: "asdf",
+			}
+
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			domain := &operatorv1alpha1.Domain{}
+			err = k8sClient.Get(ctx, typeNamespacedName, domain)
+			Expect(err).To(HaveOccurred())
+			Expect(errors.IsNotFound(err)).To(BeTrue())
+		})
+	})
+})
+
+func mailuMock() string {
+	mux := chi.NewMux()
+	// get domains
+	mux.Get("/domain", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("get request: %+v\n", r)
+		response := []mailu.DomainDetails{
+			mailu.DomainDetails{
+				Name: "example.com",
+			},
+			mailu.DomainDetails{
+				Name: "foo.example.com",
+			},
+		}
+
+		body, err := jsoniter.Marshal(response)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(fmt.Sprintf("{\"error\":\"%v\"}", err)))
+			return
+		}
+
+		_, err = w.Write(body)
+		//require.NoError(t, err)
+	})
+
+	// get specific domain
+	mux.Get("/domain/example.com", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("get request: %+v\n", r)
+
+		domain := mailu.DomainDetails{
+			Name: "example.com",
+		}
+		body, err := jsoniter.Marshal(domain)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(fmt.Sprintf("{\"error\":\"%v\"}", err)))
+			return
+		}
+
+		_, err = w.Write(body)
+		//require.NoError(t, err)
+	})
+
+	mux.Get("/domain/nonexistent.com", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("get request: %+v\n", r)
+
+		body, err := jsoniter.Marshal(`{"code": 0, "message": "Not found"}`)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(fmt.Sprintf("{\"error\":\"%v\"}", err)))
+			return
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+		_, err = w.Write(body)
+		//require.NoError(t, err)
+	})
+
+	// create domain
+	mux.Post("/domain", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("post request: %+v\n", r)
+
+		body, err := jsoniter.Marshal(`{"code": 0, "message": "ok"}`)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(fmt.Sprintf("{\"error\":\"%v\"}", err)))
+			return
+		}
+
+		_, err = w.Write(body)
+	})
+
+	// update domain
+	mux.Patch("/domain/example.com", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("patch request: %+v\n", r)
+
+		body, err := jsoniter.Marshal(`{"code": 0, "message": "ok"}`)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(fmt.Sprintf("{\"error\":\"%v\"}", err)))
+			return
+		}
+
+		_, err = w.Write(body)
+	})
+
+	// delete domain
+	mux.Delete("/domain/example.com", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("delete request: %+v\n", r)
+
+		body, err := jsoniter.Marshal(`{"code": 0, "message": "ok"}`)
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte(fmt.Sprintf("{\"error\":\"%v\"}", err)))
+			return
+		}
+
+		_, err = w.Write(body)
+	})
+
+	httpSrv := httptest.NewServer(mux)
+	//t.Cleanup(func() { httpSrv.Close() })
+
+	return httpSrv.URL
+}
---
+++ pkg/mailu/client.go
@@ -0,0 +1,449 @@
+package mailu
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/oapi-codegen/runtime"
+	openapi_types "github.com/oapi-codegen/runtime/types"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+// Alias defines model for Alias.
+type Alias struct {
+	// Comment a comment
+	Comment     *string   `json:"comment,omitempty"`
+	Destination *[]string `json:"destination,omitempty"`
+
+	// Email the alias email address
+	Email string `json:"email"`
+
+	// Wildcard enable SQL Like wildcard syntax
+	Wildcard *bool `json:"wildcard,omitempty"`
+}
+
+// Domain defines model for Domain.
+type Domain struct {
+	Alternatives *[]string `json:"alternatives,omitempty"`
+
+	// Comment a comment
+	Comment *string `json:"comment,omitempty"`
+
+	// MaxAliases maximum number of aliases
+	MaxAliases *int `json:"max_aliases,omitempty"`
+
+	// MaxQuotaBytes maximum quota for mailbox
+	MaxQuotaBytes *int `json:"max_quota_bytes,omitempty"`
+
+	// MaxUsers maximum number of users
+	MaxUsers *int `json:"max_users,omitempty"`
+
+	// Name FQDN (e.g. example.com)
+	Name string `json:"name"`
+
+	// SignupEnabled allow signup
+	SignupEnabled *bool `json:"signup_enabled,omitempty"`
+}
+
+type DomainDetails struct {
+	Alternatives *[]string `json:"alternatives,omitempty"`
+
+	// Comment a comment
+	Comment *string `json:"comment,omitempty"`
+
+	DNSAutoconfig *[]string `json:"dns_autoconfig,omitempty"`
+
+	//DnsMX
+	//DnsSPF
+	//DnsDKIM
+	//DnsDMARC
+	//DnsDMARCReport
+	//DnsTLSA
+
+	// MaxAliases maximum number of aliases
+	MaxAliases *int `json:"max_aliases,omitempty"`
+
+	// MaxQuotaBytes maximum quota for mailbox
+	MaxQuotaBytes *int `json:"max_quota_bytes,omitempty"`
+
+	// MaxUsers maximum number of users
+	MaxUsers *int `json:"max_users,omitempty"`
+
+	// Managers lists managers of this domain
+	Managers *[]string `json:"managers,omitempty"`
+
+	// Name FQDN (e.g. example.com)
+	Name string `json:"name"`
+
+	// SignupEnabled allow signup
+	SignupEnabled *bool `json:"signup_enabled,omitempty"`
+}
+
+// User defines model for UserGet.
+type User struct {
+	// AllowSpoofing Allow the user to spoof the sender (send email as anyone)
+	AllowSpoofing *bool `json:"allow_spoofing,omitempty"`
+
+	// ChangePwNextLogin Force the user to change their password at next login
+	ChangePwNextLogin *bool `json:"change_pw_next_login,omitempty"`
+
+	// Comment A description for the user. This description is shown on the Users page
+	Comment *string `json:"comment,omitempty"`
+
+	// DisplayedName The display name of the user within the Admin GUI
+	DisplayedName *string `json:"displayed_name,omitempty"`
+
+	// Email The email address of the user
+	Email *string `json:"email,omitempty"`
+
+	// EnableImap Allow email retrieval via IMAP
+	EnableImap *bool `json:"enable_imap,omitempty"`
+
+	// EnablePop Allow email retrieval via POP3
+	EnablePop *bool `json:"enable_pop,omitempty"`
+
+	// Enabled Enable the user. When an user is disabled, the user is unable to login to the Admin GUI or webmail or access his email via IMAP/POP3 or send mail
+	Enabled            *bool     `json:"enabled,omitempty"`
+	ForwardDestination *[]string `json:"forward_destination,omitempty"`
+
+	// ForwardEnabled Enable auto forwarding
+	ForwardEnabled *bool `json:"forward_enabled,omitempty"`
+
+	// ForwardKeep Keep a copy of the forwarded email in the inbox
+	ForwardKeep *bool `json:"forward_keep,omitempty"`
+
+	// GlobalAdmin Make the user a global administrator
+	GlobalAdmin *bool `json:"global_admin,omitempty"`
+
+	// Password Hash of the user's password; Example='$bcrypt-sha256$v=2,t=2b,r=12$fmsAdJbYAD1gGQIE5nfJq.$zLkQUEs2XZfTpAEpcix/1k5UTNPm0jO'
+	Password *string `json:"password,omitempty"`
+
+	// QuotaBytes The maximum quota for the user’s email box in bytes
+	QuotaBytes *int `json:"quota_bytes,omitempty"`
+
+	// QuotaBytesUsed The size of the user’s email box in bytes
+	QuotaBytesUsed *int `json:"quota_bytes_used,omitempty"`
+
+	// ReplyBody The body of the automatic reply email
+	ReplyBody *string `json:"reply_body,omitempty"`
+
+	// ReplyEnabled Enable automatic replies. This is also known as out of office (ooo) or out of facility (oof) replies
+	ReplyEnabled *bool `json:"reply_enabled,omitempty"`
+
+	// ReplyEnddate End date for automatic replies in YYYY-MM-DD format.
+	ReplyEnddate *openapi_types.Date `json:"reply_enddate,omitempty"`
+
+	// ReplyStartdate Start date for automatic replies in YYYY-MM-DD format.
+	ReplyStartdate *openapi_types.Date `json:"reply_startdate,omitempty"`
+
+	// ReplySubject Optional subject for the automatic reply
+	ReplySubject *string `json:"reply_subject,omitempty"`
+
+	// SpamEnabled Enable the spam filter
+	SpamEnabled *bool `json:"spam_enabled,omitempty"`
+
+	// SpamMarkAsRead Enable marking spam mails as read
+	SpamMarkAsRead *bool `json:"spam_mark_as_read,omitempty"`
+
+	// SpamThreshold The user defined spam filter tolerance
+	SpamThreshold *int `json:"spam_threshold,omitempty"`
+}
+
+// RequestEditorFn  is the function signature for the RequestEditor callback function
+type RequestEditorFn func(ctx context.Context, req *http.Request) error
+
+// Doer performs HTTP requests.
+//
+// The standard http.Client implements this interface.
+type HttpRequestDoer interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+// Client which conforms to the OpenAPI3 specification for this service.
+type Client struct {
+	// The endpoint of the server conforming to this interface, with scheme,
+	// https://api.deepmap.com for example. This can contain a path relative
+	// to the server, such as https://api.deepmap.com/dev-test, and all the
+	// paths in the swagger spec will be appended to the server.
+	Server string
+
+	// Doer for performing requests, typically a *http.Client with any
+	// customized settings, such as certificate chains.
+	Client HttpRequestDoer
+
+	// A list of callbacks for modifying requests which are generated before sending over
+	// the network.
+	RequestEditors []RequestEditorFn
+}
+
+// ClientOption allows setting custom parameters during construction
+type ClientOption func(*Client) error
+
+// Creates a new Client, with reasonable defaults
+func NewClient(server string, opts ...ClientOption) (*Client, error) {
+	// create a client with sane default values
+	client := Client{
+		Server: server,
+	}
+	// mutate client and add all optional params
+	for _, o := range opts {
+		if err := o(&client); err != nil {
+			return nil, err
+		}
+	}
+	// ensure the server URL always has a trailing slash
+	if !strings.HasSuffix(client.Server, "/") {
+		client.Server += "/"
+	}
+	// create httpClient, if not already present
+	if client.Client == nil {
+		client.Client = &http.Client{}
+	}
+	return &client, nil
+}
+
+// WithHTTPClient allows overriding the default Doer, which is
+// automatically created using http.Client. This is useful for tests.
+func WithHTTPClient(doer HttpRequestDoer) ClientOption {
+	return func(c *Client) error {
+		c.Client = doer
+		return nil
+	}
+}
+
+// WithRequestEditorFn allows setting up a callback function, which will be
+// called right before sending the request. This can be used to mutate the request.
+func WithRequestEditorFn(fn RequestEditorFn) ClientOption {
+	return func(c *Client) error {
+		c.RequestEditors = append(c.RequestEditors, fn)
+		return nil
+	}
+}
+
+func (c *Client) FindDomain(ctx context.Context, domain string, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	req, err := NewFindDomainRequest(c.Server, domain)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	if err := c.applyEditors(ctx, req, reqEditors); err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+func (c *Client) CreateDomain(ctx context.Context, body Domain, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	req, err := NewCreateDomainRequest(c.Server, body)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	if err := c.applyEditors(ctx, req, reqEditors); err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+func (c *Client) UpdateDomain(ctx context.Context, domain string, body Domain, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	req, err := NewUpdateDomainRequest(c.Server, domain, body)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	if err := c.applyEditors(ctx, req, reqEditors); err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+func (c *Client) DeleteDomain(ctx context.Context, domain string, reqEditors ...RequestEditorFn) (*http.Response, error) {
+	req, err := NewDeleteDomainRequest(c.Server, domain)
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	if err := c.applyEditors(ctx, req, reqEditors); err != nil {
+		return nil, err
+	}
+	return c.Client.Do(req)
+}
+
+// NewFindDomainRequest generates requests for FindDomain
+func NewFindDomainRequest(server string, domain string) (*http.Request, error) {
+	var err error
+
+	var pathParam0 string
+
+	pathParam0, err = runtime.StyleParamWithLocation("simple", false, "domain", runtime.ParamLocationPath, domain)
+	if err != nil {
+		return nil, err
+	}
+
+	serverURL, err := url.Parse(server)
+	if err != nil {
+		return nil, err
+	}
+
+	operationPath := fmt.Sprintf("/domain/%s", pathParam0)
+	if operationPath[0] == '/' {
+		operationPath = "." + operationPath
+	}
+
+	queryURL, err := serverURL.Parse(operationPath)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("GET", queryURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+// NewCreateDomainRequest calls the generic CreateDomain builder with application/json body
+func NewCreateDomainRequest(server string, body Domain) (*http.Request, error) {
+	var bodyReader io.Reader
+	buf, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	bodyReader = bytes.NewReader(buf)
+	return NewCreateDomainRequestWithBody(server, "application/json", bodyReader)
+}
+
+// NewCreateDomainRequestWithBody generates requests for CreateDomain with any type of body
+func NewCreateDomainRequestWithBody(server string, contentType string, body io.Reader) (*http.Request, error) {
+	var err error
+
+	serverURL, err := url.Parse(server)
+	if err != nil {
+		return nil, err
+	}
+
+	operationPath := fmt.Sprintf("/domain")
+	if operationPath[0] == '/' {
+		operationPath = "." + operationPath
+	}
+
+	queryURL, err := serverURL.Parse(operationPath)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", queryURL.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", contentType)
+
+	return req, nil
+}
+
+// NewUpdateDomainRequest calls the generic UpdateDomain builder with application/json body
+func NewUpdateDomainRequest(server string, domain string, body Domain) (*http.Request, error) {
+	var bodyReader io.Reader
+	buf, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	bodyReader = bytes.NewReader(buf)
+	return NewUpdateDomainRequestWithBody(server, domain, "application/json", bodyReader)
+}
+
+// NewUpdateDomainRequestWithBody generates requests for UpdateDomain with any type of body
+func NewUpdateDomainRequestWithBody(server string, domain string, contentType string, body io.Reader) (*http.Request, error) {
+	var err error
+
+	var pathParam0 string
+
+	pathParam0, err = runtime.StyleParamWithLocation("simple", false, "domain", runtime.ParamLocationPath, domain)
+	if err != nil {
+		return nil, err
+	}
+
+	serverURL, err := url.Parse(server)
+	if err != nil {
+		return nil, err
+	}
+
+	operationPath := fmt.Sprintf("/domain/%s", pathParam0)
+	if operationPath[0] == '/' {
+		operationPath = "." + operationPath
+	}
+
+	queryURL, err := serverURL.Parse(operationPath)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PATCH", queryURL.String(), body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", contentType)
+
+	return req, nil
+}
+
+// NewDeleteDomainRequest generates requests for DeleteDomain
+func NewDeleteDomainRequest(server string, domain string) (*http.Request, error) {
+	var err error
+
+	var pathParam0 string
+
+	pathParam0, err = runtime.StyleParamWithLocation("simple", false, "domain", runtime.ParamLocationPath, domain)
+	if err != nil {
+		return nil, err
+	}
+
+	serverURL, err := url.Parse(server)
+	if err != nil {
+		return nil, err
+	}
+
+	operationPath := fmt.Sprintf("/domain/%s", pathParam0)
+	if operationPath[0] == '/' {
+		operationPath = "." + operationPath
+	}
+
+	queryURL, err := serverURL.Parse(operationPath)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("DELETE", queryURL.String(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func (c *Client) applyEditors(ctx context.Context, req *http.Request, additionalEditors []RequestEditorFn) error {
+	for _, r := range c.RequestEditors {
+		if err := r(ctx, req); err != nil {
+			return err
+		}
+	}
+	for _, r := range additionalEditors {
+		if err := r(ctx, req); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// JSONError wraps an error json response.
+func JSONError(rw http.ResponseWriter, c int, m string) {
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(c)
+	_, _ = rw.Write([]byte(fmt.Sprintf(`{"errors": [{"title": "error", "detail": "%s"}]}`, m)))
+}
